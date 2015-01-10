2327cbda2a208d04769e6653c937f9824a9cec26
user <user@users-Mac.local>
2015-01-09 21:45:23 -0800

    magic

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var sqlite3 = require('sqlite3').verbose();
 var fs = require("fs");
-var file = '/Users/camh/Library/Messages/chat.db';
+var file = '/Users/user/Library/Messages/chat.db';
+var express = require("express");
+var bodyparser = require("body-parser");
 var exists = fs.existsSync(file);
 if (!exists) {
 	console.log('neg');
@@ -8,28 +10,25 @@ if (!exists) {
 var db = new sqlite3.Database(file);
 console.log(db);
 
-db.serialize(function() {
-	//db.run("CREATE TABLE lorem (info TEXT)");
-
-	/*
-	var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-	for (var i = 0; i < 10; i++) {
-		stmt.run("Ipsum " + i);
-	}
-	stmt.finalize();
-	*/
-
-	db.each("SELECT message.text, handle.id as handle_name FROM message JOIN handle ON message.handle_id = handle.ROWID WHERE message.service = 'iMessage'", function(err, row) {
-			// console.log(err);
-			// console.log(row);
-			console.log(row.handle_name + ": " + row.text);
+var app = express();
+app.use(bodyparser.json());
+app.get('/all_messages', function(req, res) {
+	db.serialize(function() {
+		db.each("SELECT message.text, message.ROWID as message_id, handle.id as handle_name FROM message JOIN handle ON message.handle_id = handle.ROWID WHERE message.service = 'iMessage'", function(err, row) {
+				// console.log(err);
+				// console.log(row);
+				console.log(row.handle_name + " #" + row.message_id + ": " + row.text);
+		});
 	});
-/*
-	db.each("SELECT id FROM handle \
-		", function(err, row) {
-		console.log(row.id);
-	//	console.log(row.handle_id + ": " + row.text);
-	});*/
+
+	db.close();
+	res.send("ok!");
+	res.end();
 });
 
-db.close();
+app.post('/send_message', function(req, res) {
+	res.send(req.body.message);
+	res.end();
+});
+
+app.listen(3000);
