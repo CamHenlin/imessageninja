6e542f918683880f1b08d10dc773c4d0b7ea0289
user <user@users-Mac.local>
2015-01-10 20:55:20 -0800

    working send / listing capabilites

--- app.js
+++ app.js
@@ -26,8 +26,21 @@ app.get('/all_messages', function(req, res) {
 	res.end();
 });
 
+var applescript = require("applescript");
 app.post('/send_message', function(req, res) {
-	res.send(req.body.message);
+	var to = req.body.to
+	var message = req.body.message;
+	applescript.execFile('send_message.AppleScript', [to, message],
+function(err, result) {
+		if (err) {
+			console.log('error');
+			console.log(err);
+		}
+
+		//res.send("sent!");
+	});
+	console.log(req);
+	//res.send(req.body.message);
 	res.end();
 });
 
